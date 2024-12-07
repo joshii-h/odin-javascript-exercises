6ab6a8cf1452e86e850ded5f38ac368d772b7188
joshii-h <joshuahirsig@gmail.com>
2024-12-07 18:07:46 +0100

    Arrays and loops done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    
+    if (x < 0 || y < 0 || typeof x !== 'number' || typeof y !== 'number') {return 'ERROR';}
 
+    let rangeArray = Array.from({length: Math.min(x,y) + Math.abs(x-y)}, (_, i) => i + Math.min(x,y));
+
+    //return rangeArray
+    return rangeArray.reduce((a,b) => a + b);
 };
 
 // Do not edit below this line
